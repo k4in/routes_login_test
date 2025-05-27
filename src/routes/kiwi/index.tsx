906fdeef9cbc69710b800fb6a8c646b31e27cc7b
k4in <>
2025-05-27 23:39:22 +0200

    adding sub routes

--- src/routes/kiwi/index.tsx
+++ src/routes/kiwi/index.tsx
@@ -1,9 +1,24 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, Link } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/kiwi/')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/kiwi/"!</div>;
+  return (
+    <div>
+      <nav className="flex gap-2 mb-10">
+        <Link to={'/kiwi'} search={{ amount: 3, origin: 'Spain' }}>
+          Option 1
+        </Link>
+        <Link to={'/kiwi'} search={{ amount: 9, origin: 'Ecuador' }}>
+          Option 2
+        </Link>
+        <Link to={'/kiwi'} search={{ amount: 47, origin: 'Bangladesh' }}>
+          Option 3
+        </Link>
+      </nav>
+      <div>halloo...</div>
+    </div>
+  );
 }
--- src/routes/pineapple/index.tsx
+++ src/routes/pineapple/index.tsx
@@ -1,9 +1,24 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/pineapple/')({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/pineapple/"!</div>
+  return (
+    <div>
+      <nav className="flex gap-2 mb-10">
+        <Link to={'/pineapple'} search={{ amount: 13, origin: 'Argentina' }}>
+          Option 1
+        </Link>
+        <Link to={'/pineapple'} search={{ amount: 8, origin: 'South Africa' }}>
+          Option 2
+        </Link>
+        <Link to={'/pineapple'} search={{ amount: 963, origin: 'Samoa' }}>
+          Option 3
+        </Link>
+      </nav>
+      <div>halloo...</div>
+    </div>
+  );
 }
